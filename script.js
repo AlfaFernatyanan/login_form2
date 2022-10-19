0d873cd682aeba0f9a4b961ade8610c2997c5a5e
alvinapt <tutuparyalvin@gmail.com>
2022-10-19 21:55:52 +0700

    menambahkan function register & login pada javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+  var datalogin = document.getElementById("login_form").elements;
+  var username = datalogin[0]["value"];
+  var password = datalogin[1]["value"];
+
+  if (username != "alfa") {
+  } else if (password != "kiki") {
+  } else {
+  }
+}
+function register() {
+  var dataregister = document.getElementById("register_form").elements;
+  var firstname = dataregister[0]["value"];
+  var lastname = dataregister[1]["value"];
+  var email = dataregister[2]["value"];
+  var password = dataregister[3]["value"];
+}
