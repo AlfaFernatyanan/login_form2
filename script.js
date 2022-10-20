1b73ec4cd0292d72874a0097a4f8d62ea8a0b6c9
alvinapt <tutuparyalvin@gmail.com>
2022-10-21 00:37:00 +0700

    fetching data untuk digunakan di karyawan.html

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ function register() {
   var email = dataregister[2]["value"];
   var password = dataregister[3]["value"];
 }
+
+async function generateListKaryawan() {
+  var jsonData = {};
+  var listStrHtml = "";
+
+  var containerListKaryawan = document.getElementById(
+    "container_list_karyawan"
+  );
+  var url = "https://dummyapi.io/data/v1/user?limit=10";
+
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "app-id": "634ff758ef6ad6104ce624bb",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => (jsonData = data));
+
+  console.log(jsonData);
+
+  for (let index = 0; index < jsonData["data"].length; index++) {
+    var res = jsonData["data"][index];
+    var picture = res["picture"];
+    var firstName = res["firstName"];
+    var lastname = res["lastName"];
+    var title = res["title"];
+    var gender = "";
+
+    if (title != "mr") {
+      gender = "Perempuan";
+    } else {
+      gender = "Laki - Laki";
+    }
+    var strHtml = `<div class="Karyawan_kontener"><img src="${picture}" /><div>  <h2>${firstName} ${lastname}</h2>   <h3>${gender}</h3></div></div>`;
+    console.log(picture);
+    listStrHtml += strHtml;
+  }
+  containerListKaryawan.innerHTML = listStrHtml;
+}
